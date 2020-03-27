77375098ea84a49c81260a01aef9220c05f3a5c6
RodolphoAchou <rodolphodealmeida@hotmail.com>
2020-03-26 22:37:19 -0300

    Final do dia 26/03/2020

--- backend/src/controllers/ProfileController.js
+++ backend/src/controllers/ProfileController.js
@@ -3,7 +3,6 @@ const connection = require('../database/connection');
 module.exports = {
     async index(request, response){
         const ong_id = request.headers.authorization;
-        console.log("entrei")
         const incidents = await connection('incidents')
         .where('ong_id', ong_id)
         .select('*');
